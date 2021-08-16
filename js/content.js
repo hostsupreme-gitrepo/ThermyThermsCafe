627b89eebdd2b8be33abcbc57166e3855b3d92be
Stanley Muhammad <littlstan@outlook.com>
2021-08-15 19:48:36 -0700

    after contacus

--- js/content.js
+++ js/content.js
@@ -23,83 +23,67 @@ const hoursinnerText = `<div class="hours">
       </ul>
     </div>`;
 
-const contactus = `	<div class="contact__content">
-					
-<!-- Section -->
-<div class="conctact__details">
-      
-
-      
-      <div class="detail--1">
-        <span class="template-icon-feature-phone-circle"></span>
-        <h2>Call</h2>
-        <p>
-          (602) 487-4885<br/>
-          <br/>
-        </p>
-      </div>
-
-      <div class="detail--2">
-        <span class="template-icon-feature-location-map"></span>
-        <h2>Visit</h2>
-        <p>
-          464 Rhode Island Av.<br/>
-          Phoenix AZ, 85041
-        </p>
-      </div>
-      
-      <!-- Right column -->
-      <div class="detail--e">
-        <span class="template-icon-feature-clock"></span>
-        <h2>Email</h2>
-        <p>
-        kitchen@thermythermscafe.com<br/>
-         
-        </p>
-      </div>
-      
-   
-
-  
-
-
-<!-- Section -->
-
-  
-  <!-- Contact form -->
-  <form name="contact-form" id="contact-form" method="POST" action="#" class="contact-form">
-         
-      <!-- Left column -->
-      <div class="form--1">
-        <div class="template-component-form-field template-state-block">
-          <label for="contact-form-name">Your Name *</label>
-          <input type="text" name="contact-form-name" id="contact-form-name"/>
+const contactus = `	
+<div class="contact__content">
+      <div class="contactbox email">
+            <div class="detail--3">
+              <span class="template-icon-feature-clock"></span>
+              <h2>Email</h2>
+              <p>
+              kitchen@thermythermscafe.com<br/>
+               
+              </p>
+            </div>
+          </div>
+            <div class="contactbox address">
+            <div class="detail--2">
+              <span class="template-icon-feature-location-map"></span>
+              <h2>Visit</h2>
+              <p>
+                464 Rhode Island Av.<br/>
+                Phoenix AZ, 85041
+              </p>
+            </div>
+          </div>
+          <div class="contactbox phone">      
+            <div class="detail--1">
+              <span class="template-icon-feature-phone-circle"></span>
+              <h2>Call</h2>
+              <p>
+                (602) 487-4885<br/>
+                <br/>
+              </p>
+            </div>
+          </div>
+      <div class="contactbox contactform">
+        <div class="form--entry">
+          <div class="template-component-form-field template-state-block">
+            <label for="contact-form-name">Your Name *</label>
+            <input type="text" name="contact-form-name" id="contact-form-name"/>
+          </div>
+          <div class="template-component-form-field template-state-block">
+            <label for="contact-form-email">Your E-mail *</label>
+            <input type="text" name="contact-form-email" id="contact-form-email"/>
+          </div>
+          <div class="template-component-form-field template-state-block">
+            <label for="contact-form-phone">Phone Number</label>
+            <input type="text" name="contact-form-phone" id="contact-form-phone"/>
+          </div>
         </div>
-        <div class="template-component-form-field template-state-block">
-          <label for="contact-form-email">Your E-mail *</label>
-          <input type="text" name="contact-form-email" id="contact-form-email"/>
+        <div class="form--message">
+          <div class="template-component-form-field template-state-block">
+            <label for="contact-form-message">Message *</label>
+            <textarea rows="1" cols="10" name="contact-form-message" id="contact-form-message"></textarea>
+          </div>
         </div>
-        <div class="template-component-form-field template-state-block">
-          <label for="contact-form-phone">Phone Number</label>
-          <input type="text" name="contact-form-phone" id="contact-form-phone"/>
-        </div>
-      </div>
       
-      <!-- Right column -->
-      <div class="form--2">
-        <div class="template-component-form-field template-state-block">
-          <label for="contact-form-message">Message *</label>
-          <textarea rows="1" cols="10" name="contact-form-message" id="contact-form-message"></textarea>
+    
+        <div class="form--button">
+          <span class="template-state-block">
+            <input type="submit" value="Submit Message" class="template-component-button" name="contact-form-submit" id="contact-form-submit"/>
+          </span>
         </div>
       </div>
-      
-  </form>  
-  </div> 
-     <!-- Button -->
-    <div class="template-align-center template-clear-fix template-margin-top-1">
-      <span class="template-state-block">
-        <input type="submit" value="Submit Message" class="template-component-button" name="contact-form-submit" id="contact-form-submit"/>
-      </span>
     </div>
     
 
@@ -123,33 +107,13 @@ const contactus = `	<div class="contact__content">
     <!-- Button -->
     <a href="#" class="template-component-google-map-button">
       <span class="template-icon-meta-marker"></span>
-      <span class="template-component-google-map-button-label-show">Show Map</span>
-      <span class="template-component-google-map-button-label-hide">Hide Map</span>
+      <span class="template-component-google-map-button-label-show"> </span>
+      <span class="template-component-google-map-button-label-hide"> </span>
     </a>
 
   </div>
 
-  <script type="text/javascript">
-
-    jQuery(document).ready(function()
-    {
-      jQuery('.template-component-google-map').templateGoogleMap(
-      {
-        coordinate		:
-        {
-          lat			:	'-37.823952',
-          lng			:	'144.958766'
-        },
-        dimension		:
-        {
-          width		:	'100%',
-          height		:	'400px'
-        },
-        marker			:	'media/image/map_pointer.png'
-      });
-    });
-
-  </script>					
+  				
 
 </div>
 
